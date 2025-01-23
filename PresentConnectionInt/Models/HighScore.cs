ae69f6070486d95fb1b697042785bbb674c60b86
sergb <kisame02@ya.ru>
2025-01-23 18:46:53 +0200

    Describe your changes here

---
+++ PresentConnectionInt/Models/HighScore.cs
@@ -0,0 +1,10 @@
+﻿namespace PresentConnectionInt.Models
+{
+    public class HighScore
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public int Score { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
